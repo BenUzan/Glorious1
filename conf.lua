6004c3842624fe2f3dc2f5051960d0b5351da6ef
Nebuza <uzanmuyumbaben@gmail.com>
2022-11-18 00:20:47 +0200

    Initialisation du projet

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(w)
+    w.width = 400
+    w.height = 400
+    w.title = "Glorious"
+end
---
+++ main.lua
@@ -0,0 +1,13 @@
+function love.load()
+    -- Everything will be loaded here
+end
+
+function love.update(dt)
+    -- Everything will be updated here
+end
+
+
+function love.draw()
+  -- Everything will be drawn here
+  love.graphics.printf('Dead Moon Studio...', 0, 200, 800,'center')
+end

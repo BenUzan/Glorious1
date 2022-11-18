e930fbf9088225587ac5bd66184072ac794f39b2
Nebuza <uzanmuyumbaben@gmail.com>
2022-11-18 17:59:45 +0200

    Definition du splash screen
    A l'aide de la librairy o-ten-one

--- main.lua
+++ main.lua
@@ -1,13 +1,31 @@
+-- The library for the LOVE rain splash screen
+local o_ten_one = require "src/lib/o-ten-one"
+
+
 function love.load()
-    -- Everything will be loaded here
+  -- Everything will be loaded here
+  splashe = o_ten_one({fill="rain", delay_after=1})
+  splashe.onDone = function ()
+    print "DONE"
+  end
 end
 
 function love.update(dt)
-    -- Everything will be updated here
+  -- Everything will be updated here
+  splashe:update(dt)
+  if love.keyboard.isDown('space') then
+    splashe:skip()
+  end
 end
-
 
 function love.draw()
   -- Everything will be drawn here
-  love.graphics.printf('Dead Moon Studio...', 0, 200, 800,'center')
+  splashe:draw()
+end
+
+function love.keypressed(key)
+  -- Recupere une touche entree au clavier
+  if key == "escape" then
+    love.event.push("quit")
+  end
 end

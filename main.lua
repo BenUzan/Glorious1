66dd6ddf39f31eec2f88bd7f77afbb7d9e159343
Nebuza <uzanmuyumbaben@gmail.com>
2022-11-19 22:51:28 +0200

    Suppression du splash screen
    Impossible de lier le jeu et le splash screen

--- main.lua
+++ main.lua
@@ -4,28 +4,23 @@ local o_ten_one = require "src/lib/o-ten-one"
 
 function love.load()
   -- Everything will be loaded here
-  splashe = o_ten_one({fill="rain", delay_after=1})
-  splashe.onDone = function ()
-    print "DONE"
-  end
+  
 end
 
 function love.update(dt)
   -- Everything will be updated here
-  splashe:update(dt)
-  if love.keyboard.isDown('space') then
-    splashe:skip()
-  end
+  
 end
 
 function love.draw()
   -- Everything will be drawn here
-  splashe:draw()
+
 end
 
 function love.keypressed(key)
   -- Recupere une touche entree au clavier
   if key == "escape" then
-    love.event.push("quit")
+    splashe:skip()
+    --love.event.push("quit")
   end
 end
